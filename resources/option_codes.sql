2a882ecd1e03012c628457de70b5e4a43ea6b63b
Michal Ferlinski <fermich@gmail.com>
2017-05-28 13:03:08 +0200

    gpw option codes

---
+++ resources/option_codes.sql
@@ -0,0 +1,27 @@
+create table option_code(code varchar(1), type varchar(1), month tinyint);
+
+insert into option_code(code, type, month) values ('A', 'C', 1);
+insert into option_code(code, type, month) values ('B', 'C', 2);
+insert into option_code(code, type, month) values ('C', 'C', 3);
+insert into option_code(code, type, month) values ('D', 'C', 4);
+insert into option_code(code, type, month) values ('E', 'C', 5);
+insert into option_code(code, type, month) values ('F', 'C', 6);
+insert into option_code(code, type, month) values ('G', 'C', 7);
+insert into option_code(code, type, month) values ('H', 'C', 8);
+insert into option_code(code, type, month) values ('I', 'C', 9);
+insert into option_code(code, type, month) values ('J', 'C', 10);
+insert into option_code(code, type, month) values ('K', 'C', 11);
+insert into option_code(code, type, month) values ('L', 'C', 12);
+insert into option_code(code, type, month) values ('M', 'P', 1);
+insert into option_code(code, type, month) values ('N', 'P', 2);
+insert into option_code(code, type, month) values ('O', 'P', 3);
+insert into option_code(code, type, month) values ('P', 'P', 4);
+insert into option_code(code, type, month) values ('Q', 'P', 5);
+insert into option_code(code, type, month) values ('R', 'P', 6);
+insert into option_code(code, type, month) values ('S', 'P', 7);
+insert into option_code(code, type, month) values ('T', 'P', 8);
+insert into option_code(code, type, month) values ('U', 'P', 9);
+insert into option_code(code, type, month) values ('V', 'P', 10);
+insert into option_code(code, type, month) values ('W', 'P', 11);
+insert into option_code(code, type, month) values ('X', 'P', 12);
+
